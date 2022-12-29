20dcfcae7aeb1a14ac118cfc8ffe79ac74d87a33
OISIN HEARNE <G00405249@atu.ie>
2022-12-29 15:22:34 +0000

    randomization of four coloured pegs, grid sets up w/ placeholder images.

--- MastermindG00405249/MastermindG00405249/MainPage.xaml.cs
+++ MastermindG00405249/MastermindG00405249/MainPage.xaml.cs
@@ -8,11 +8,71 @@ using Xamarin.Forms;
 
 namespace MastermindG00405249
 {
+    //Empty= 411C15
+    //Bg= 6b3520
     public partial class MainPage : ContentPage
     {
+        ImageButton _peg;
+        Random _rng = new Random();
+        int _currentGuess = 1;
+        int[] _generatedGuess = new int[4];
+
+
         public MainPage()
         {
             InitializeComponent();
+            StartGame();
+
+            _peg = new ImageButton();
+            _peg.HeightRequest = 76;
+            _peg.WidthRequest = 76;
+            _peg.CornerRadius = 38;
+        }
+
+        private void StartGame()
+        {
+            SetUpGrid();
+            ComputerGuess();
+
+
+        }
+
+        private void ComputerGuess()
+        {
+            ImageButton b;
+            for(int i = 0; i < 4; i ++)
+            {
+                b = new ImageButton();
+                _generatedGuess[i] = _rng.Next(1, 6);
+
+                b.Source = _generatedGuess[i] + ".png";
+                b.HorizontalOptions = LayoutOptions.Center;
+                b.VerticalOptions = LayoutOptions.Center;
+                b.IsVisible = true;
+
+                MainGrid.Children.Add(b);
+                b.SetValue(Grid.RowProperty, 1);
+                b.SetValue(Grid.ColumnProperty, 3+i);
+            }
+        }
+
+        private void SetUpGrid()
+        {
+            Image b;
+            for(int r = 3; r < 13; r++)
+                for(int c = 3; c < 7; c++)
+                {
+                    b = new Image();
+
+                    b.Source = "empty.png";
+                    b.HorizontalOptions = LayoutOptions.Center;
+                    b.VerticalOptions = LayoutOptions.Center;
+                    b.IsEnabled = true;
+
+                    MainGrid.Children.Add(b);
+                    b.SetValue(Grid.RowProperty, r);
+                    b.SetValue(Grid.ColumnProperty, c);
+                }
         }
     }
 }
